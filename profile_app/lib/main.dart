7445c60acfe484816d7d05c48751415442f5d82a
umumararungu <umcynth@gmail.com>
2024-11-28 11:56:19 +0300

    changed

--- profile_app/lib/main.dart
+++ profile_app/lib/main.dart
@@ -8,8 +8,18 @@ void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: UserProfileScreen(),
+    );
+  }
+}
 class UserProfileScreen extends StatelessWidget {
-  UserProfileScreen({Key? key}) : super(key: key);
+  UserProfileScreen({super.key});
 
   final UserController controller = Get.find();
   final TextEditingController nameController = TextEditingController();
